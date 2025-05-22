e625ab802bcab97d0ae2f937ee6f627428cbe31b
Yohanan7n <156466410+Yohanan7n@users.noreply.github.com>
2025-05-22 17:30:56 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const x = process.argv[2];
 let count = parseInt(x);
 let output = '';
 
-if (isNaN(count)) {
+if (isNaN(count) || count < 1) {
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < count; i++) {
